9d66b71b0027789fc9bb6be2ef20a1ae4c22da61
Vika Hamaliy <vikahamaliy@gmail.com>
2023-07-14 16:23:06 +0300

    add HW2

---
+++ main.js
@@ -0,0 +1,38 @@
+const cel = 20;
+const far = cel + 32;
+console.log("Far:>>>", far)
+
+const days = 31;
+const hours = days * 24;
+const minuts = days * 24 * 60;
+console.log("Hours:>>>", hours)
+console.log("Minuts:>>>", minuts)
+
+let heal = 100;
+let energy = 100;
+heal -= 50;
+energy -= 60;
+console.log("Heal:>>>", heal)
+console.log("Energy:>>>", energy)
+
+const price = 150;
+const result = price / 100 * 10;
+console.log("Discount:>>>", result)
+
+const number = 10.837;
+console.log("Math.floor:>>>", Math.floor(number))
+
+const str = "16.349";
+console.log("ParseFloat:>>>", parseFloat(str))
+
+const string = "4";
+console.log("ParseInt:>>>", parseInt(string))
+
+const num = 15;
+console.log("Math.sqrt:>>>", Math.sqrt(num))
+
+const numStr = "40"
+const numberValue = 20;
+console.log("ParseInt:>>>", parseInt(numStr))
+console.log("ToString:>>>", numberValue.toString())
+
